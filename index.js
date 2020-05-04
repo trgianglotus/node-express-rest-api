9b6ccaa83cc33bc57d68a085332a3b593e0e6693
trgianglotus <hoangtruonggiangnguyen@gmail.com>
2020-05-04 21:10:47 +0800

    resolve vulnerabilities

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import jsonwebtoken from "jsonwebtoken";
+import helmet from "helmet";
 import routes from "./src/routes/crmRoutes";
 
 const app = express();
@@ -10,13 +11,14 @@ const PORT = 3000;
 // mongoose connection
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/CRMdb", {
-  useMongoClient: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(helmet());
 // JWT setup
 app.use((req, res, next) => {
   if (
